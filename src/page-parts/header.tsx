3d007353874e9db63764873bd583ff6c33c81832
Yuri Zhvirblis <zhvirblis@protonmail.com>
2021-09-15 23:41:42 +0300

    add toggler

--- src/page-parts/header.tsx
+++ src/page-parts/header.tsx
@@ -3,6 +3,11 @@ export default function Header() {
         <nav className="navbar navbar-dark" style={{backgroundColor: "#54910b"}}>
             <div className="container">
                 <a className="navbar-brand">Word trainer</a>
+	    <button className="navbar-toggler"
+		type="button" data-bs-toggle="collapse" data-bs-target="#nacbarNav"
+	    >
+		<span className="navbar-toggler-icon"></span>
+	    </button>
             </div>
         </nav>
     )
