0e155489aafba0cb11e5b6b7e73d8d48c4f6a87e
zhvirblis <11149023+zhvirblis@users.noreply.github.com>
2021-10-09 20:21:18 +0300

    created module list mark up

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./App";
 import store from "./redux";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
+import App from "./App";
 
 ReactDOM.render(
     <Provider store={store}>
--- src/pages/home/parts/ModuleList.tsx
+++ src/pages/home/parts/ModuleList.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { moduleSelector } from "../../../redux/slices/indexedDB/modules";
 import { moduleActions } from "../../../redux/slices/indexedDB/modules";
+import { StoreStatus } from "../../../redux/slices/indexedDB";
 import "./styles.css";
 
 export default function ModuleList() {
@@ -9,7 +10,45 @@ export default function ModuleList() {
     useEffect(() => {
         dispatch(moduleActions.get());
     }, []);
-    const some = useSelector(moduleSelector);
-    console.log("--moduleSelector", some);
-    return <div>Module list</div>;
+    const modules = useSelector(moduleSelector);
+    return (
+        <div className="modules-list">
+            {modules.status === StoreStatus.Loading && (
+                <div className="card-body message">Loading...</div>
+            )}
+            {modules.status === StoreStatus.Updating && (
+                <div className="card-body message">Updating...</div>
+            )}
+            {modules.status === StoreStatus.Done && (
+                <div className="list-group list-group-flush">
+                    {modules.list.map((module: any) => (
+                        <div
+                            className="module-item list-group-item"
+                            key={module.id}
+                        >
+                            <div>
+                                <h3>
+                                    <a href="#">{module.name}</a>
+                                </h3>
+                                <div className="btns-wrapper">
+                                    <button
+                                        type="button"
+                                        className="btn btn-outline-primary btn-sm"
+                                    >
+                                        Rename
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-outline-danger btn-sm"
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
